de9d1fe4dedc6ef8c1f653b93f2236ac3b9da2b0
BuiltInRenderPipeline <built-in-render-pipeline@users.noreply.github.com>
2026-06-12 20:04:55 +0800

    测试

--- Assets/Scripts/Game/Test/Comps/CompTest.cs
+++ Assets/Scripts/Game/Test/Comps/CompTest.cs
@@ -2,13 +2,14 @@
 using Scripts.Framework.Core.Timer;
 using UnityEngine;
 
+// 测试组件：测试 MonoBehaviour 的生命周期和 TimerMgr 的 CallLater 功能。
 namespace Game.Test.Comps
 {
     public class CompTest : MonoBehaviour
     {
         void Start()
         {
-            GameLog.Info("CompTest Start");ƒ
+            GameLog.Info("CompTest Start");
             TimerMgr.Inst.CallLater(  () =>
             {
                 GameLog.Info("CompTest TimerMgr.Inst.CallLater Time.frameCount：" + Time.frameCount);
